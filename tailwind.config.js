c92fd5d86511ae22a141366d86c1f3dd4791bec6
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-04 21:48:35 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'weather-primary': '#261972',
+        'weather-secondary': '#3725a4'
+      }
+    },
+    fontFamily: {
+      Roboto: ['Roboto, sans-serif']
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    }
+  },
+  plugins: []
+}
