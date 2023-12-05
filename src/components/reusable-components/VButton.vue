6b0a3511931685ea4f7e63150077a9a85782a2b3
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-05 21:00:09 +0100

    Add VButton and VDialog components

---
+++ src/components/reusable-components/VButton.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { computed, useSlots } from 'vue'
+import LoadAnimation from '../../assets/svg/LoadAnimation.vue'
+
+export interface VButtonProps {
+  label?: string
+  type?: ButtonType
+  color?: 'white' | 'neutral'
+  disabled?: boolean
+  loading?: boolean
+}
+
+export type ButtonType = 'button' | 'submit' | 'reset'
+
+const props = withDefaults(defineProps<VButtonProps>(), {
+  label: 'Continue',
+  type: 'button',
+  color: undefined,
+  disabled: false,
+  loading: false
+})
+
+const slots = useSlots()
+const hasIcon = computed<boolean>(() => !!slots?.icon)
+
+const white = computed(() => props.color === 'white')
+const neutral = computed(() => props.color === 'neutral')
+const basic = computed(() => !props.color)
+</script>
+
+<template>
+  <button
+    :type="type"
+    class="inline-flex justify-center rounded-full px-4 py-2 text-sm font-medium shadow-md transition focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    :class="[
+      {
+        'border-slate-700 bg-white text-black hover:opacity-70 active:opacity-80': white
+      },
+      {
+        'border-neutral-700 bg-neutral-600 text-white hover:bg-neutral-500 active:bg-neutral-600':
+          neutral
+      },
+      {
+        'border-weather-primary bg-weather-secondary hover:bg-brand-300 active:bg-weather-primary text-white':
+          basic
+      }
+    ]"
+    :disabled="disabled"
+  >
+    <template v-if="hasIcon">
+      <slot name="icon"> </slot>
+    </template>
+    <LoadAnimation class="w-5 h-5" v-if="loading"></LoadAnimation>
+    <span v-else>
+      {{ label }}
+    </span>
+  </button>
+</template>
---
+++ src/components/reusable-components/VDialog.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel } from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+import VButtonIcon from '../reusable-components/VButtonIcon.vue'
+
+defineOptions({
+  inheritAttrs: false
+})
+
+const props = defineProps<{
+  modelValue: boolean
+}>()
+
+const showDialog = computed({
+  get() {
+    return props.modelValue
+  },
+  set(newValue: boolean) {
+    emit('update:modelValue', newValue)
+  }
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
+</script>
+
+<template>
+  <TransitionRoot appear :show="showDialog" as="template">
+    <Dialog as="div" class="relative z-10" @close="showDialog = false">
+      <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="relative min-w-[200px] transform rounded-2xl bg-white p-3 text-left align-middle shadow-xl transition-all sm:min-w-[450px]"
+              v-bind="$attrs"
+            >
+              <div class="absolute top-1 right-1 z-10">
+                <VButtonIcon @click="showDialog = false">
+                  <XMarkIcon class="h-5 w-5"></XMarkIcon>
+                </VButtonIcon>
+              </div>
+              <slot></slot>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
