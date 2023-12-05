b8aa58493dacb306a14d195dc71f3ba212bb4ce8
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-05 02:04:26 +0100

    Create VButtonIcon.vue

---
+++ src/components/reusableComponents/VButtonIcon.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    disabled?: boolean
+  }>(),
+  {
+    disabled: false
+  }
+)
+
+const emit = defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
+
+<template>
+  <button
+    v-bind="$attrs"
+    type="button"
+    class="grid h-8 w-8 place-items-center rounded-full hover:cursor-pointer hover:opacity-60 active:opacity-80 disabled:pointer-events-none disabled:opacity-50"
+    :disabled="disabled"
+    @click="emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
