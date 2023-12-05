bde470127d260d8037def13bb9762b787570db7a
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-05 02:05:20 +0100

    Create NavigationBar.vue and add it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    <NavigationBar />
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import NavigationBar from './components/NavigationBar.vue'
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="bg-weather-primary sticky top-0 shadow-lg">
+    <nav class="flex gap-6 items-center shadow-lg justify-between container text-white py-6">
+      <RouterLink :to="{ name: 'home' }" class="flex items-center">
+        <SunIcon class="w-7 h-7" />
+        <span class="ml-2 text-lg font-bold">Weather Application</span>
+      </RouterLink>
+      <VButtonIcon>
+        <InformationCircleIcon class="w-7 h-7" />
+      </VButtonIcon>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { SunIcon, InformationCircleIcon } from '@heroicons/vue/24/outline'
+import VButtonIcon from './reusableComponents/VButtonIcon.vue'
+</script>
