c8e6253a74e56e5b5b71952a0e5726a20b73d9f0
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-05 02:02:44 +0100

    Update tailwind config, add axios, heroicons, headlessUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,16 @@ export default {
         'weather-secondary': '#3725a4'
       }
     },
-    fontFamily: {
-      Roboto: ['Roboto, sans-serif']
-    },
     screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px'
+    },
+    container: {
+      padding: '2rem',
+      center: true
     }
   },
   plugins: []
