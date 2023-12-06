428e0353de60a3bd6b0b80c0bccd11ed7b38bc79
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-06 21:44:59 +0100

    Remove redundant shadow from NavBar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-weather-primary sticky top-0 shadow-lg">
-    <nav class="flex gap-6 items-center shadow-lg justify-between container text-white py-6">
+    <nav class="flex gap-6 items-center justify-between container text-white py-6">
       <RouterLink :to="{ name: 'home' }" class="flex items-center">
         <SunIcon class="w-7 h-7" />
         <span class="ml-2 text-lg font-bold">Weather Application</span>
