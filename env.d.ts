ff5693e99e7f022f2ea7bb25ef8a8d52bf036eb7
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-08 00:38:03 +0100

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   // Api keys
-  readonly VITE_MAPBOX_API_KEY: string
   readonly VITE_LOCATIONIQ_API_KEY: string
+  // Base url
+  readonly VITE_BASE_LOCATION_URL: string
+  readonly VITE_BASE_WEATHER_URL: string
 }
