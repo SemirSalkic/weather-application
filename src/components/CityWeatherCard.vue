bf0a78a6bcfe9a8f6ab7a83f23dd52d8a2c647f9
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-10 16:49:15 +0100

    Add navigate and click functionality in CityCard

--- src/components/CityWeatherCard.vue
+++ src/components/CityWeatherCard.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
 import { convertTimestampToDate, capitalizeFirstLetters } from '../util/index'
-import type { WeatherData } from '@/stores/types'
+import type { WeatherData, Location } from '@/stores/types'
 import VButtonIcon from '@/components/action-components/VButtonIcon.vue'
 import ConfirmationDialog from '@/components/action-components/ConfirmationDialog.vue'
-import { PlusCircleIcon, MinusCircleIcon } from '@heroicons/vue/24/outline'
+import { PlusCircleIcon, MinusCircleIcon, ArrowRightCircleIcon } from '@heroicons/vue/24/outline'
 import { useWeatherStore } from '@/stores/weather'
 import { storeToRefs } from 'pinia'
 import { computed, ref } from 'vue'
+import { useLocationStore } from '@/stores/location'
+import router, { RouteName } from '@/router'
 
 const props = withDefaults(
   defineProps<{
     currentWeather: WeatherData
-    selectedLocationName: string
+    location: Location
+    locationName: string
     isClickable?: boolean
   }>(),
   {
-    isClickable: false //TODO: Napravi da se moze kliknuti na karticu
+    isClickable: false
   }
 )
 
 const weatherStore = useWeatherStore()
 const { locationWeatherList } = storeToRefs(weatherStore)
+const { selectedLocation, selectedLocationName } = storeToRefs(useLocationStore())
 
 const removeListItemDialog = ref(false)
 
@@ -35,6 +39,17 @@ function removeListItem() {
   weatherStore.removeLocationWeatherItem(props.currentWeather.id)
   removeListItemDialog.value = false
 }
+
+function navigateToSelectedCity() {
+  selectedLocation.value = props.location
+  selectedLocationName.value = props.locationName
+  const country = props.location.address.country.replace(/\s/g, '-').toLocaleLowerCase()
+  const city = props.location.address.name.replace(/\s/g, '-').toLocaleLowerCase()
+  router.push({
+    name: RouteName.CityWeatherView,
+    params: { country: country, city: city }
+  })
+}
 </script>
 
 <template>
@@ -42,20 +57,27 @@ function removeListItem() {
     v-if="currentWeather"
     class="container mx-auto rounded-lg bg-weather-primary px-4 py-4 text-white"
   >
-    <div class="relative mb-2 flex flex-col rounded-lg bg-weather-secondary px-4 py-4">
-      <span class="text-xl font-bold">{{ selectedLocationName }}</span>
-      <span>Time: {{ convertTimestampToDate(currentWeather?.dt) }}</span>
-      <div class="absolute right-2 top-3 flex">
-        <VButtonIcon
-          v-if="!isInLocationWeatherList"
-          @click="weatherStore.addLocationWeatherItem(currentWeather, selectedLocationName)"
-        >
-          <PlusCircleIcon class="h-8 w-8"></PlusCircleIcon>
-        </VButtonIcon>
-        <VButtonIcon v-else @click="removeListItemDialog = true">
-          <MinusCircleIcon class="h-8 w-8 hover:text-red-600"></MinusCircleIcon>
-        </VButtonIcon>
+    <div class="mb-2 flex flex-col rounded-lg bg-weather-secondary px-4 py-4">
+      <div class="flex items-start justify-between">
+        <span class="text-xl font-bold">{{ locationName }}</span>
+        <div class="flex">
+          <VButtonIcon
+            v-if="!isInLocationWeatherList"
+            @click="
+              weatherStore.addLocationWeatherItem(location, currentWeather, selectedLocationName)
+            "
+          >
+            <PlusCircleIcon class="h-8 w-8"></PlusCircleIcon>
+          </VButtonIcon>
+          <VButtonIcon v-else @click="removeListItemDialog = true">
+            <MinusCircleIcon class="h-8 w-8 hover:text-red-600"></MinusCircleIcon>
+          </VButtonIcon>
+          <VButtonIcon v-if="isClickable" @click="navigateToSelectedCity">
+            <ArrowRightCircleIcon class="h-8 w-8"></ArrowRightCircleIcon>
+          </VButtonIcon>
+        </div>
       </div>
+      <span>Time: {{ convertTimestampToDate(currentWeather?.dt) }}</span>
     </div>
     <div class="flex w-full items-center justify-around rounded-lg bg-weather-secondary px-4 py-4">
       <div class="flex flex-col items-center">
