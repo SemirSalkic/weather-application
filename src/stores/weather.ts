e9518815112dd14c4041026988fae74596c517fc
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-10 04:01:57 +0100

    Add locationWeatherList with add, remove functions

--- src/stores/weather.ts
+++ src/stores/weather.ts
@@ -1,12 +1,28 @@
 import axios from 'axios'
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import type { Forecast, WeatherData } from './types'
+import type { Forecast, WeatherData, WeatherDataWithLocation } from './types'
 import { STORE_WEATHER, VITE_BASE_WEATHER_URL, VITE_OPENWEATHER_API_KEY } from '@/util/const'
+import { useLocalStorage } from '@vueuse/core'
 
 export const useWeatherStore = defineStore(STORE_WEATHER, () => {
   const currentWeatherData = ref<WeatherData | null>(null)
   const forecastData = ref<Forecast | null>(null)
+  const locationWeatherList = useLocalStorage<WeatherDataWithLocation[]>(
+    'weatherDataWithLocationList',
+    []
+  )
+
+  function removeLocationWeatherItem(id: number) {
+    locationWeatherList.value = locationWeatherList.value?.filter(
+      (item) => item.cityWeatherData.id !== id
+    )
+  }
+
+  function addLocationWeatherItem(cityWeatherData: WeatherData, locationName: string) {
+    const item = { cityWeatherData, locationName }
+    locationWeatherList.value?.push(item)
+  }
 
   async function fetchWeatherData(lat: string, lon: string) {
     if (!lat || !lon) return
@@ -34,5 +50,13 @@ export const useWeatherStore = defineStore(STORE_WEATHER, () => {
     }
   }
 
-  return { currentWeatherData, forecastData, fetchWeatherData, fetchForecastWeatherData }
+  return {
+    currentWeatherData,
+    forecastData,
+    locationWeatherList,
+    addLocationWeatherItem,
+    removeLocationWeatherItem,
+    fetchWeatherData,
+    fetchForecastWeatherData
+  }
 })
