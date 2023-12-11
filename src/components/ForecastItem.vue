84f95cd011c3bee3a3086b125765545fd307f448
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-11 23:09:31 +0100

    update forecastItem text size

--- src/components/ForecastItem.vue
+++ src/components/ForecastItem.vue
@@ -20,16 +20,16 @@ withDefaults(
       v-if="!title"
       class="grid grid-cols-5 items-center justify-around gap-2 rounded-lg bg-weather-secondary px-2 text-center"
     >
-      <span class="font-semibold md:text-lg">{{ timestampToTime(forecastData?.dt) }}</span>
-      <span class="font-semibold">{{
+      <span class="text-xs font-semibold md:text-lg">{{ timestampToTime(forecastData?.dt) }}</span>
+      <span class="text-xs font-semibold md:text-base">{{
         forecastData?.main?.temp ? Math.round(forecastData.main.temp) + '°C' : ''
       }}</span>
-      <span class="text-sm">{{
+      <span class="text-xs md:text-sm">{{
         capitalizeFirstLetters(forecastData?.weather[0]?.description)
       }}</span>
-      <div class="col-span-2 flex flex-col">
-        <span class="text-sm">Humidity: {{ forecastData?.main?.humidity }}%</span>
-        <span class="text-sm">Wind Speed: {{ forecastData?.wind?.speed }}km/h</span>
+      <div class="col-span-2 flex flex-col text-xs md:text-sm">
+        <span>Humidity: {{ forecastData?.main?.humidity }}%</span>
+        <span>Wind Speed: {{ forecastData?.wind?.speed }}km/h</span>
       </div>
     </div>
     <div v-else class="text-center text-lg font-semibold">
