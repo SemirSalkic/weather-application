d647cf4b9f35011faf742011af2a3a1e7880966e
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-13 02:22:46 +0100

    Refactor ForecastItem

--- src/components/ForecastItem.vue
+++ src/components/ForecastItem.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import type { ForecastListItem } from '@/stores/types'
 import { timestampToTime, capitalizeFirstLetters } from '@/util'
+import { computed } from 'vue'
 
-withDefaults(
+const props = withDefaults(
   defineProps<{
     forecastData?: ForecastListItem
   }>(),
@@ -10,6 +11,18 @@ withDefaults(
     forecastData: undefined
   }
 )
+
+const iconUrl = computed(
+  () => `http://openweathermap.org/img/wn/${props.forecastData?.weather[0].icon}.png`
+)
+const temperature = computed(() =>
+  props.forecastData?.main?.temp ? Math.round(props.forecastData.main.temp) + '°C' : ''
+)
+const description = computed(() =>
+  capitalizeFirstLetters(props.forecastData?.weather[0]?.description)
+)
+const humidity = computed(() => props.forecastData?.main?.humidity)
+const windSpeed = computed(() => props.forecastData?.wind?.speed)
 </script>
 
 <template>
@@ -18,20 +31,12 @@ withDefaults(
       class="grid grid-cols-5 items-center justify-around gap-2 rounded-lg bg-weather-secondary text-center"
     >
       <span class="text-xs font-semibold md:text-lg">{{ timestampToTime(forecastData?.dt) }}</span>
-      <span class="text-xs font-semibold md:text-base">{{
-        forecastData?.main?.temp ? Math.round(forecastData.main.temp) + '°C' : ''
-      }}</span>
-      <img
-        class="h-auto w-16 justify-self-center"
-        :src="`http://openweathermap.org/img/wn/${forecastData?.weather[0].icon}.png`"
-        :alt="forecastData?.weather[0].description"
-      />
-      <span class="hidden text-xs md:block md:text-sm">{{
-        capitalizeFirstLetters(forecastData?.weather[0]?.description)
-      }}</span>
+      <span class="text-xs font-semibold md:text-base">{{ temperature }}</span>
+      <img class="h-auto w-16 justify-self-center" :src="iconUrl" :alt="description" />
+      <span class="hidden text-xs md:block md:text-sm">{{ description }}</span>
       <div class="col-span-2 flex flex-col text-xs md:col-span-1 md:text-sm">
-        <span>Humidity: {{ forecastData?.main?.humidity }}%</span>
-        <span>Wind Speed: {{ forecastData?.wind?.speed }}km/h</span>
+        <span>Humidity: {{ humidity }}%</span>
+        <span>Wind Speed: {{ windSpeed }}km/h</span>
       </div>
     </div>
   </div>
