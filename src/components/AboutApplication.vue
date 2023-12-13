0342c85d19b685900215495e0371e304bd928ace
SemirSalkic2439 <semirsalkic2439@gmail.com>
2023-12-13 01:50:14 +0100

    Update AboutApplication.vue

--- src/components/AboutApplication.vue
+++ src/components/AboutApplication.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { PlusCircleIcon, MinusCircleIcon } from '@heroicons/vue/24/outline'
+import { PlusCircleIcon, MinusCircleIcon, SunIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -10,7 +10,7 @@ import { PlusCircleIcon, MinusCircleIcon } from '@heroicons/vue/24/outline'
       platform to keep track of the weather conditions in your favorite cities.
     </p>
     <p class="mt-4 text-2xl font-semibold">How it Works:</p>
-    <ol class="mt-2 flex list-inside list-decimal flex-col gap-2 text-lg">
+    <ol class="mt-2 flex list-decimal flex-col gap-2 text-lg">
       <li>
         <strong>City Search:</strong> Begin by typing the name of the city you're interested into
         the search bar. Our application will provide a list of matching results.
@@ -27,11 +27,29 @@ import { PlusCircleIcon, MinusCircleIcon } from '@heroicons/vue/24/outline'
         home page, allowing you to access its weather information anytime.
       </li>
       <li>
-        <strong>Removing a City:</strong> If you decide to stop tracking a city, just navigate to
-        the home page and look for the city card of the city you want to remove. In the top right
-        corner of the city card, you'll see a
-        <MinusCircleIcon class="inline h-6 w-6" h-6 w-6”></MinusCircleIcon> icon. Simply click on
-        this icon to stop tracking the city.
+        <strong>Removing a City:</strong> If you decide to stop tracking a city, you can do it from
+        either the home page or while looking at a specific city.
+        <ul class="list-inside list-disc">
+          <li>
+            <strong>From the Home Page:</strong> Look for the city card of the city you want to
+            remove. In the top right corner of the city card, you'll see a
+            <MinusCircleIcon class="inline h-6 w-6" h-6 w-6”></MinusCircleIcon> icon. Simply click
+            on this icon to stop tracking the city.
+          </li>
+          <li>
+            <strong>From a Specific City's Page:</strong> If you're currently viewing a specific
+            city's weather, you can also remove the city from there. Look for the
+            <MinusCircleIcon class="inline h-6 w-6" h-6 w-6”></MinusCircleIcon> icon, located near
+            the city's name in the top right corner of the city card. Clicking on this icon will
+            stop tracking the city.
+          </li>
+        </ul>
+      </li>
+      <li>
+        <strong>Navigating Back to the Home Page:</strong> To go back to the home page at any time,
+        simply click on the "<SunIcon class="inline h-6 w-6" /> Weather Application" logo, located
+        in the top left corner of the page. This will take you back to the home page where you can
+        see all your tracked cities.
       </li>
     </ol>
     <p class="mt-6 text-lg">
