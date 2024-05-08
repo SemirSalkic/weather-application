20fb076f81e25e7902925bd592426dc6dc657bb6
SemirSalkic <semirsalkic2439@gmail.com>
2024-05-08 18:33:35 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavigationBar from '@/components/NavigationBar.vue'
+</script>
+
 <template>
   <main class="min-h-screen bg-weather-secondary">
     <NavigationBar />
     <RouterView />
   </main>
 </template>
-
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-import NavigationBar from '@/components/NavigationBar.vue'
-</script>
 
 <style>
 html,
